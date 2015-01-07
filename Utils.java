f757b74889236113ea1dd10eceb0c3f55c79972b
Alexander Chiu <alexanderchiu@outlook.com>
2015-01-07 15:23:37 +0200

    Added gamma function to Utils class

--- Utils.java
+++ Utils.java
@@ -80,23 +80,37 @@ public class Utils {
 
         return x;
     }
+    
+/**
+ * [Lanczos approximation of gamma function
+ * @param  x Input value
+ * @return  a Value of gamma function at x
+ */
+
+    public static double gamma(double x) {
+        int g = 7;
+        double[] p = {0.99999999999980993, 676.5203681218851, -1259.1392167224028,
+                      771.32342877765313, -176.61502916214059, 12.507343278686905,
+                      -0.13857109526572012, 9.9843695780195716e-6, 1.5056327351493116e-7
+                     };
+
+        if (x < 0.5) {
+            return Math.PI / (Math.sin(Math.PI * x) * gamma(1 - x));
+        }
+
+        x -= 1;
+        double a = p[0];
+        double t = x + g + 0.5;
+        for (int i = 1; i < p.length; i++) {
+            a += p[i] / (x + i);
+        }
+
+        return Math.sqrt(2 * Math.PI) * Math.pow(t, x + 0.5) * Math.exp(-t) * a;
+    }
 
     public static void main(String[] args) {
 
-        Double[] xdata = { -0.034804, 0.079101, 0.723332, 0.16598};
-
-        ComplexNumber[] x = new ComplexNumber[xdata.length];
-
-        for (int i = 0; i < xdata.length; i++) {
-            x[i] = new ComplexNumber(xdata[i]);
-            // System.out.println(x[i]);
-        }
-
-        ComplexNumber[] X = fft(x);
-
-        ComplexNumber[] y = ifft(X);
-        System.out.println(Arrays.toString(x));
-        System.out.println(Arrays.toString(X));
-        System.out.println(Arrays.toString(y));
+        double x = gamma(1.5);
+        System.out.println(x);
     }
 }
