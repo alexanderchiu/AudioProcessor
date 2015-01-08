62730789e204e82094afe9124ce587b5436ebe11
Alexander Chiu <alexanderchiu@outlook.com>
2015-01-08 12:07:52 +0200

    Implemented VAD function

--- Denoiser.java
+++ Denoiser.java
@@ -2,22 +2,27 @@ import java.util.Arrays;
 
 public class Denoiser implements AudioProcessor {
 
-    private static int windowLength = 256;
-    private static double overlapRatio = 0.5;
+    private static int windowLength;
+    private static double overlapRatio;
     private int fs;
-    private double noSpeechDuration  = 0.4;
+    private double noSpeechDuration;
     private int noSpeechSegments;
     private boolean speechFlag;
     private boolean noiseFlag;
     private int noiseCounter;
     private int noiseLength;
 
-    public Denoiser(int fs, double noSpeechDuration) {
+    public Denoiser(int fs, double noSpeechDuration, int noiseLength) {
+        windowLength = 256;
+        overlapRatio = 0.5;
+
         this.fs = fs;
         this.noSpeechDuration = noSpeechDuration;
         this.noSpeechSegments = (int)Math.floor((noSpeechDuration * fs - windowLength) / (overlapRatio * windowLength) + 1);
-
-        System.out.println(this.noSpeechSegments);
+        this.speechFlag = false;
+        this.noiseFlag = false;
+        this.noiseCounter = 0;
+        this.noiseLength = noiseLength;
     }
 
     public double[] process(double[] input) {
@@ -50,20 +55,73 @@ public class Denoiser implements AudioProcessor {
         }
 
         double[][] noise = new double[frames][noSpeechSegments];
-       
-        noise  = Arrays.copyOfRange(signalFFTMagnitude, 0, noSpeechSegments);
-        double[] noiseMean = Utils.mean(noise,0);
-        
-        // System.out.println(Arrays.toString(noiseMean));
-        // System.out.println(noiseMean.length);
 
-        
+        noise  = Arrays.copyOfRange(signalFFTMagnitude, 0, noSpeechSegments);
+
+
+        double[] noiseMean = Utils.mean(noise, 0);
+
+        for (int i = 0; i < noSpeechSegments; i++) {
+            for (int k = 0; k < windowLength; k++) {
+                noise[i][k] = Math.pow(noise[i][k], 2);
+            }
+        }
+
+        double[] noiseVar = Utils.mean(noise, 0);
+
+        double gamma1p5 = Utils.gamma(1.5);
+        double[] G = new double[windowLength];
+        double[] gamma = new double[windowLength];
+
+        Arrays.fill(G, 1);
+        Arrays.fill(gamma, 1);
+
+        double[][] enhancedSpectrum = new double[frames][windowLength];
+
+
+
+
+
+
+
+
+        System.out.println(Arrays.toString(G));
+
+
+
         double[] enhanced = {};
 
         return enhanced;
 
     }
 
+    public void vad(double[] frame, double[] noise, int noiseCounter, int noiseThreshold, int frameReset) {
+
+        double[] spectralDifference = new double[windowLength];
+
+        for (int i = 0; i < windowLength; i++) {
+            spectralDifference[i] = 20 * (Math.log10(frame[i]) - Math.log10(noise[i]));
+            if (spectralDifference[i] < 0) {
+                spectralDifference[i] = 0;
+            }
+        }
+
+        double diff = Utils.mean(spectralDifference);
+
+        if (diff < noiseThreshold) {
+            noiseFlag = true;
+            noiseCounter += 1;
+        } else {
+            noiseFlag = false;
+            noiseCounter = 0;
+        }
+
+        if (noiseCounter > frameReset) {
+            speechFlag = false;
+        } else {
+            speechFlag = true;
+        }
+    }
 
     /**
      * Windows sampled signal using overlapping Hamming windows
@@ -73,7 +131,7 @@ public class Denoiser implements AudioProcessor {
      * @return seg The overlapping windowed segments
      */
 
-    private static double[][] segmentSignal(double[] ss, int ww, double or ) {
+    private double[][] segmentSignal(double[] ss, int ww, double or ) {
         int len = ss.length;
         double d = 1 - or;
         int frames = (int)(Math.floor((len - ww) / ww / d));
@@ -101,7 +159,7 @@ public class Denoiser implements AudioProcessor {
      * @return   reconstructedSignal Speech signal post speech denoising
      */
 
-    private static double[] overlapAndAdd(double[][] segments, double or ) {
+    private double[] overlapAndAdd(double[][] segments, double or ) {
         int ww = segments.length;
         int frames = segments[0].length;
         int start = 0;
